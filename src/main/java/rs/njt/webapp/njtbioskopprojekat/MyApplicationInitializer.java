32ef3fb7de19de594eb875cae7092485bb5b1b52
negrahodzic <negra.hodzic@gmail.com>
2020-09-04 17:10:23 +0200

    Dodat WebFilter

--- src/main/java/rs/njt/webapp/njtbioskopprojekat/MyApplicationInitializer.java
+++ src/main/java/rs/njt/webapp/njtbioskopprojekat/MyApplicationInitializer.java
@@ -12,7 +12,6 @@ import org.springframework.web.WebApplicationInitializer;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 import rs.njt.webapp.njtbioskopprojekat.config.MyWebApplicationContextConfig;
-import rs.njt.webapp.njtbioskopprojekat.entity.GenreEntity;
 
 
 /**
@@ -20,76 +19,24 @@ import rs.njt.webapp.njtbioskopprojekat.entity.GenreEntity;
  * @author remmi
  */
 public class MyApplicationInitializer implements WebApplicationInitializer {
-    
+
     @Override
     public void onStartup(ServletContext sc) throws ServletException {
         System.out.println("====================== MyApplicationInitializer ===============================");
         AnnotationConfigWebApplicationContext webApplicationContext = new AnnotationConfigWebApplicationContext();
         webApplicationContext.register(MyWebApplicationContextConfig.class);
         webApplicationContext.setServletContext(sc);
-
+        
+//        FilterRegistration.Dynamic hiddenHttpMethodFilter = sc.addFilter("hiddenHttpMethodFilter", new HiddenHttpMethodFilter());
+//        hiddenHttpMethodFilter.addMappingForUrlPatterns(EnumSet.allOf(DispatcherType.class), true, "/*");
+        
         //konfigurisati DispatcherServlet
         ServletRegistration.Dynamic dispatcherServlet = sc.addServlet("myDispatcherServlet", new DispatcherServlet(webApplicationContext));
         dispatcherServlet.addMapping("/");
         dispatcherServlet.setLoadOnStartup(1);
-        GenreEntity GenreServiceImpl;
-        
-        /*
-        MovieServiceImpl movieServiceImpl = new MovieServiceImpl();
-        Movie movie = movieServiceImpl.getById(1L);
-        
-        List<Movie> listMovies = new ArrayList<Movie>();
-        listMovies.add(movie);
-        sc.setAttribute("movies", listMovies);
-//       */ 
-//        List<Projection> listProjections = createProjections();
-//        sc.setAttribute("projections", listProjections);
-//        GenreServiceImpl gs = null;
-//        gs.getAll();
-//
-//        List<Genre> gList = gs.getAll();
-//        System.out.println("===============================");
-//        System.out.println("===============================");
-//        System.out.println("===============================");
-//        System.out.println("===============================");
-//        System.out.println("GenreEntity: " + gList.get(0).toString());
-//        System.out.println("===============================");
-//        System.out.println("===============================");
-//        System.out.println("===============================");
 
-    
-    /*
-    private List<Movie> createMovies() {
-        
-       
-        
-        return new ArrayList<Movie>() {
-            {   
-                add(movieServiceImpl.getById(1L));
-//                add(new Movie("Godfather", "This is godfather movie.", 160, "Crime"));
-//                add(new Movie("Joker", "This is Joker movie.", 122, "Triller"));
-//                add(new Movie("Parasit", "This is Parasit movie.", 132, "Comedy"));
-//                add(new Movie("Godfather", "This is godfather movie.", 160, "Crime"));
-//                add(new Movie("Joker", "This is Joker movie.", 122, "Triller"));
-//                add(new Movie("Parasit", "This is Parasit movie.", 132, "Comedy"));
-            }
-//
-        };
-    }*/
-//    private List<Projection> createProjections() {
-//        return new ArrayList<Projection>() {
-//            {
-//                add(new Projection(new Date(2020, 8, 10, 18, 30), "3D", "titlovano"));
-//                add(new Projection(new Date(2020, 8, 10, 18, 30), "3D", "sihronizovano"));
-//                add(new Projection(new Date(2020, 8, 10, 18, 30), "3D", "/"));
-//                add(new Projection(new Date(2020, 8, 10, 18, 30), "3D", "titlovano"));
-//                add(new Projection(new Date(2020, 8, 10, 18, 30), "3D", "sihronizovano"));
-//                add(new Projection(new Date(2020, 8, 10, 18, 30), "3D", "/"));
-//
-//            }
-//
-//        };
-//    }
+
+
     }
 
 }
---
+++ src/main/java/rs/njt/webapp/njtbioskopprojekat/MyWebFilter.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rs.njt.webapp.njtbioskopprojekat;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import org.springframework.stereotype.Component;
+
+/**
+ *
+ * @author Negra
+ */
+@Component
+@WebFilter( urlPatterns = {"/searchMovies/*", "/searchProjections/*", "/myReservations/*",
+                            "/editProfile/*", "/logout/*"})
+public class MyWebFilter implements Filter{
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest req = (HttpServletRequest) request;
+        if (req.getSession().getAttribute("loggedUser") == null) {
+            req.getSession().setAttribute("message", "You must login first!");
+            req.getRequestDispatcher("/").forward(request, response);
+        } else {
+            chain.doFilter(request, response);
+        }
+    }
+
+    @Override
+    public void destroy() {
+    }
+    
+}
